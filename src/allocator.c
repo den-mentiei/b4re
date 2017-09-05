ee2c8455a097037effd425fa1cc4091f1cb14eaf
Den Mentiei <denis@goortom.com>
2017-09-05 20:24:58 +0300

    allocator: Added.

---
+++ src/allocator.c
@@ -0,0 +1,17 @@
+#include "allocator.h"
+
+#include <stdlib.h> // realloc
+
+static void* std_realloc(void* ctx, void* p, size_t new_size) {
+	return realloc(p, new_size);
+}
+
+static allocator_t main = {.realloc = std_realloc, .payload = NULL};
+
+allocator_t* allocator_main() {
+	return &main;
+}
+
+allocator_t* allocator_debug() {
+	return &main;
+}
---
+++ src/allocator.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stddef.h>
+
+typedef struct {
+	void* (*realloc)(void* ctx, void* p, size_t new_size);
+	void* payload;
+} allocator_t;
+
+#define BR_ALLOC(a, s)       (a)->realloc((a)->payload, NULL, (s));
+#define BR_REALLOC(a, p, ns) (a)->realloc((a)->payload, p, (ns));
+#define BR_FREE(a, p)        (a)->realloc((a)->payload, (p), 0);
+
+allocator_t* allocator_main();
+allocator_t* allocator_debug();
