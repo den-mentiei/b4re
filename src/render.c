e748c5cc7e5e6142fb3edd3d85f4b191fef8b5af
Den Mentiei <denis@goortom.com>
2017-10-12 21:54:42 +0300

    render: Debug names & dummy white texture.

--- src/render.c
+++ src/render.c
@@ -9,6 +9,7 @@ static struct {
 	bgfx_vertex_decl_t    vdecl;
 	bgfx_program_handle_t program;
 	bgfx_uniform_handle_t u_texture;
+	bgfx_texture_handle_t white_texture;
 } s_ctx = {0};
 
 void render_init() {
@@ -19,10 +20,16 @@ void render_init() {
 	bgfx_vertex_decl_end(&s_ctx.vdecl);
 
 	bgfx_shader_handle_t vs = bgfx_create_shader(bgfx_make_ref(tex_color_vs, sizeof(tex_color_vs)));
+	bgfx_set_shader_name(vs, "tex_color_vs");
 	bgfx_shader_handle_t fs = bgfx_create_shader(bgfx_make_ref(tex_color_fs, sizeof(tex_color_fs)));
+	bgfx_set_shader_name(fs, "tex_color_fs");
 	s_ctx.program = bgfx_create_program(vs, fs, true);
 
 	s_ctx.u_texture = bgfx_create_uniform("s_texture", BGFX_UNIFORM_TYPE_INT1, 1);
+
+	static uint32_t white = 1;
+	s_ctx.white_texture = bgfx_create_texture_2d(1, 1, false, 0, BGFX_TEXTURE_FORMAT_RGBA8, BGFX_TEXTURE_U_MIRROR | BGFX_TEXTURE_W_MIRROR | BGFX_TEXTURE_MAG_POINT | BGFX_TEXTURE_MIN_POINT, bgfx_make_ref(&white, sizeof(white)));
+	bgfx_set_texture_name(s_ctx.white_texture, "dummy white texture");
 }
 
 void render_shutdown() {
