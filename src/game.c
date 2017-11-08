c54ec62731ebcaf6a9ec5aa5d745ea3090eb9102
Den Mentiei <denis@goortom.com>
2017-11-08 23:52:49 +0200

    assets: Avatars added.

--- src/game.c
+++ src/game.c
@@ -46,9 +46,9 @@ bool game_update(uint16_t width, uint16_t height, float dt) {
 void game_render(uint16_t width, uint16_t height, float dt) {
 	render_sprite(assets_sprites()->travel_map.atlas_tiled_grass, 32.0f, 32.0f);
 
-	const struct sprite_t* cs = assets_sprites()->travel_map.button_compass_n;
+	const struct sprite_t* cs = assets_sprites()->avatars.avatar_man1;
 	if (imgui_button(1, cs, 128.0f, 128.0f, 64.0f, 64.0f)) {
-		log_info("Compass pressed.\n");
+		log_info("man1 pressed.\n");
 	}
 }
 
