fef48bde8389308b5e40583093ab484242ae6bc5
Den Mentiei <denis@goortom.com>
2017-11-11 21:46:09 +0200

    Minor cleanup.

--- src/imgui.h
+++ src/imgui.h
@@ -8,6 +8,4 @@ struct sprite_t;
 void imgui_update();
 void imgui_post_update();
 
-// TODO: auto gen id? pass caption to gen id as in dear imgui?
-
 bool imgui_button(int64_t id, const struct sprite_t* s, float x, float y, int w, int h);
