29f78dfe39cc65e44468ea59b4cec803402f550b
Den Mentiei <denis@goortom.com>
2017-11-15 22:12:04 +0200

    travel map: Basic scrolling.

--- src/states/travel_map.c
+++ src/states/travel_map.c
@@ -4,6 +4,7 @@
 #include <stdio.h> // snprintf
 
 #include "game.h"
+#include "log.h"
 #include "session.h"
 #include "input.h"
 #include "render.h"
@@ -11,8 +12,161 @@
 #include "render_text.h"
 #include "generated/assets.h"
 
+static struct {
+	float start_x;
+	float start_y;
+	float last_x;
+	float last_y;
+
+	float map_x;
+	float map_y;
+	int   tile_x;
+	int   tile_y;
+
+	bool scrolling;
+} s_ctx;
+
+typedef struct rect_t {
+	float x, y;
+	float w, h;
+} rect_t;
+
+static inline bool is_in_rect(float x, float y, const rect_t* rect) {
+	assert(rect);
+
+	return x >= rect->x && x < rect->x + rect->w
+		&& y >= rect->y && y < rect->y + rect->h;
+}
+
+static void start_scroll() {
+	assert(!s_ctx.scrolling);
+
+	float x, y;
+	input_position(&x, &y);
+
+	s_ctx.start_x   = x;
+	s_ctx.start_y   = y;
+	s_ctx.last_x    = x;
+	s_ctx.last_y    = y;
+	s_ctx.scrolling = true;
+}
+
+static void stop_scroll() {
+	assert(s_ctx.scrolling);
+	s_ctx.scrolling = false;
+}
+
+static const size_t VIEW_TILES   = 8;
+static const size_t PLANE_TILES  = 256;
+static const float  TILE         = 64.0f;
+static const float  VIEW_SIZE    = VIEW_TILES * TILE;
+
+static void update_scroll() {
+	float x, y;
+	input_position(&x, &y);
+
+	const float dx = x - s_ctx.last_x;
+	const float dy = y - s_ctx.last_y;
+
+	float new_x  = s_ctx.map_x + dx;
+	float new_y  = s_ctx.map_y + dy;
+	int   new_tx = s_ctx.tile_x;
+	int   new_ty = s_ctx.tile_y;
+
+	if (new_x < -TILE) {
+		new_x  += TILE;
+		new_tx += 1;
+	} else if (new_x + VIEW_SIZE > VIEW_SIZE) {
+		new_x  -= TILE;
+		new_tx -= 1;
+	}
+	if (new_y < -TILE) {
+		new_y  += TILE;
+		new_ty += 1;
+	} else if (new_y + VIEW_SIZE > VIEW_SIZE) {
+		new_y  -= TILE;
+		new_ty -= 1;
+	}
+
+	// Snapping.
+	if (new_tx < 0) {
+		new_x  = 0;
+		new_tx = 0;
+	} else if (new_tx + VIEW_TILES > PLANE_TILES) {
+		new_x  = -TILE;
+		new_tx = PLANE_TILES - VIEW_TILES;
+	}
+
+	if (new_ty < 0) {
+		new_y  = 0;
+		new_ty = 0;
+	} else if (new_ty + VIEW_TILES > PLANE_TILES) {
+		new_y  = -TILE;
+		new_ty = PLANE_TILES - VIEW_TILES;
+	}
+
+	s_ctx.map_x  = new_x;
+	s_ctx.map_y  = new_y;
+	s_ctx.tile_x = new_tx;
+	s_ctx.tile_y = new_ty;
+
+	s_ctx.last_x = x;
+	s_ctx.last_y = y;
+}
+
+static void render_scroll() {
+	const render_text_t DEBUG_TEXT = {
+		.font    = "regular",
+		.size_pt = 16.0f,
+		.color   = render_color(255, 255, 255),
+		.align   = RENDER_TEXT_ALIGN_CENTER | RENDER_TEXT_ALIGN_MIDDLE
+	};
+
+	char buf[64];
+
+	const float ox = 32.0f;
+	const float oy = 32.0f;
+							  
+	for (size_t i = 0; i < VIEW_TILES; ++i) {
+		for (size_t j = 0; j < VIEW_TILES; ++j) {
+			const size_t tx = s_ctx.tile_x + i;
+			const size_t ty = s_ctx.tile_y + j;
+			const float  x  = ox + s_ctx.map_x + TILE * i;
+			const float  y  = oy + s_ctx.map_y + TILE * j;
+
+			snprintf(buf, 64, "%zu,%zu", tx, ty);
+			render_sprite(assets_sprites()->common.sign_green_dark, x, y);
+			render_text(buf, x + TILE * 0.5f, y + TILE * 0.5f, &DEBUG_TEXT);
+		}
+	}
+
+	const float gx = ox + s_ctx.map_x;
+	const float gy = oy + s_ctx.map_y;
+	render_sprite(assets_sprites()->travel_map.mapnet, gx,          gy);
+	render_sprite(assets_sprites()->travel_map.mapnet, gx,          gy + 256.0f);
+	render_sprite(assets_sprites()->travel_map.mapnet, gx + 256.0f, gy);
+	render_sprite(assets_sprites()->travel_map.mapnet, gx + 256.0f, gy + 256.0f);
+}
+
+void states_travel_map_init() {
+}
+
 void states_travel_map_update(uint16_t width, uint16_t height, float dt) {
-	if (input_button_pressed(INPUT_BUTTON_LEFT)) {
+	if (!s_ctx.scrolling && input_button_pressed(INPUT_BUTTON_LEFT)) {
+		start_scroll();
+		return;
+	}
+
+	if (s_ctx.scrolling) {
+		if (input_button_released(INPUT_BUTTON_LEFT)) {
+			stop_scroll();
+		} else {
+			update_scroll();
+		}
+		return;
+	}
+
+	if (input_button_released(INPUT_BUTTON_LEFT)) {
 		if (session_current()) {
 			session_end();
 		}
@@ -214,15 +368,17 @@ static void resources_render() {
 
 static void render_movement() {
 	const uint8_t x = 3, y = 3;
-	render_sprite(assets_sprites()->avatars.avatar_man2, 32.0f + 64.0f * x, 32.0f + 64.0f * y);
+	/* render_sprite(assets_sprites()->avatars.avatar_man2, 32.0f + 64.0f * x, 32.0f + 64.0f * y); */
 }
 
 void states_travel_map_render(uint16_t width, uint16_t height, float dt) {
 	if (!session_current()) return;
 
 	// TODO: Render map view.
-	render_sprite(assets_sprites()->travel_map.atlas_tiled_warfog, 32.0f, 32.0f);
+	render_sprite(assets_sprites()->travel_map.atlas_tiled_grass, 32.0f, 32.0f);
 	render_movement();
+
+	render_scroll();
 
 	// Map chrome.
 	render_sprite(assets_sprites()->travel_map.black_map_frame,    0.0f,  0.0f);
