d7efe6b9431e07d410656ad710a0caf75f4941d8
Den Mentiei <denis@goortom.com>
2017-12-01 00:08:41 +0200

    log: Cleanup.

--- src/log.c
+++ src/log.c
@@ -4,24 +4,27 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-#define PRINT_TO(s, f)    \
-	va_list args;         \
-	va_start(args, f);    \
-	vfprintf(s, f, args); \
+#define PRINT_TO(s, f)        \
+	va_list args;             \
+	va_start(args, (f));      \
+	vfprintf((s), (f), args); \
 	va_end(args);
 
+#define PRINT_ENDL(s) fprintf((s), "\n");
+
 void log_info(const char* format, ...) {
-	PRINT_TO(stdout, format);
-	fprintf(stdout, "\n");
+	PRINT_TO  (stdout, format);
+	PRINT_ENDL(stdout);
 }
 
 void log_error(const char* format, ...) {
-	PRINT_TO(stderr, format);
-	fprintf(stderr, "\n");
+	PRINT_TO  (stderr, format);
+	PRINT_ENDL(stderr);
 }
 
 void log_fatal(const char* format, ...) {
-	PRINT_TO(stderr, format);
+	PRINT_TO  (stderr, format);
+	PRINT_ENDL(stderr);
 	fflush(NULL);
 	exit(1);
 }
